ec86f031be6b1d3f07e5ad840dc28b3b6198beca
kongbomb <kongbomb.coder@gmail.com>
2014-03-14 11:23:15 +0900

    add license. improve anchor position. create download section

--- app/org/psjava/site/PsjavaSiteController.java
+++ app/org/psjava/site/PsjavaSiteController.java
@@ -1,7 +1,6 @@
 package org.psjava.site;
 
 import japa.parser.JavaParser;
-import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
 import japa.parser.ast.body.JavadocComment;
@@ -9,30 +8,32 @@ import japa.parser.ast.stmt.BlockStmt;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
+import java.util.Map;
+import java.util.TreeMap;
 
 import models.Item;
 
-import org.psjava.ds.Collection;
+import org.json.JSONArray;
 import org.psjava.ds.array.Array;
 import org.psjava.ds.array.DynamicArray;
 import org.psjava.site.util.StringUtil;
 import org.psjava.site.util.Util;
-import org.psjava.site.util.ZipUtil;
 import org.psjava.util.AssertStatus;
 import org.psjava.util.DataKeeper;
 import org.psjava.util.Pair;
+import org.psjava.util.Triple;
 import org.psjava.util.ZeroTo;
 
-import play.api.Play;
+import play.cache.Cache;
+import play.libs.F.Function;
+import play.libs.F.Function0;
+import play.libs.F.Promise;
+import play.libs.WS;
+import play.libs.WS.Response;
+import play.libs.WS.WSRequestHolder;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.detail;
@@ -40,149 +41,174 @@ import views.html.index;
 
 public class PsjavaSiteController extends Controller {
 
+	private static final HashMap<String, String> EMPTY_PARAM = new HashMap<String, String>();
+	private static final String ALGO_LIST_URL = constructApiUrl("src/test/java/org/psjava/example/algo");
+	private static final String DS_LIST_URL = constructApiUrl("src/test/java/org/psjava/example/ds");
+	private static final String EXAMPLE_PATH = "src/test/java/org/psjava/example";
 	private static final String REF_NAME = "master";
-	private static final String PATH_PREFIX = "psjava-master/";
-	private static final String EXAMPLE_PATH_IN_ZIP = PATH_PREFIX + "src/test/java/org/psjava/example";
+	private static final String GITHUB_RAW_ROOT = "https://raw.github.com/psjava/psjava/" + REF_NAME;
+	private static final String GITHUB_PAGE_ROOT = "https://github.com/psjava/psjava/blob/" + REF_NAME;
 	private static final String EXAMPLE_FILE_SUFFIX = "Example.java";
-	private static final String DS_PATH_PREFIX = EXAMPLE_PATH_IN_ZIP + "/ds/";
-	private static final String ALGO_PATH_PREFIX = EXAMPLE_PATH_IN_ZIP + "/algo/";
 
-	public static Result index() throws IOException {
-		File zipFile = getZipFile();
-		String version = extractVersionFromFile(zipFile);
-		List<Item> ds = extractItems(zipFile, DS_PATH_PREFIX);
-		List<Item> algo = extractItems(zipFile, ALGO_PATH_PREFIX);
-		return ok(index.render(version, ds, algo));
+	@SuppressWarnings("unchecked")
+	public static Promise<Result> index() {
+		Promise<Response> pom = createUrlFetchPromise(GITHUB_RAW_ROOT + "/pom.xml", EMPTY_PARAM);
+		Promise<Response> ds = createUrlFetchPromise(DS_LIST_URL, createRefParam());
+		Promise<Response> algo = createUrlFetchPromise(ALGO_LIST_URL, createRefParam());
+		return Promise.sequence(pom, ds, algo).map(new Function<List<Response>, Result>() {
+			@Override
+			public Result apply(List<Response> list) throws Throwable {
+				String pomFileText = list.get(0).getBody();
+				String version = extractVersion(pomFileText);
+				List<Item> ds = extractItems(list.get(1));
+				List<Item> algo = extractItems(list.get(2));
+				return ok(index.render(version, ds, algo));
+			}
+		});
 	}
 
-	private static String extractVersionFromFile(File zipFile) throws ZipException, IOException, UnsupportedEncodingException {
-		String text = ZipUtil.loadUTF8StringInZipFileOrNull(zipFile, PATH_PREFIX + "pom.xml");
+	private static String constructApiUrl(String dirPath) {
+		return "https://api.github.com/repos/psjava/psjava/contents/" + dirPath;
+	}
+
+	private static String extractVersion(String pomFileText) {
 		String start = "<version>";
-		int s = text.indexOf(start);
-		int e = text.indexOf("</version>");
-		return text.substring(s + "<version>".length(), e);
+		int s = pomFileText.indexOf(start);
+		int e = pomFileText.indexOf("</version>");
+		return pomFileText.substring(s + "<version>".length(), e);
 	}
 
-	private static File getZipFile() {
-		return Play.getFile("file-resources/psjava-master.zip", Play.current());
+	public static Promise<Result> showDs(String id) {
+		return showDetail("ds", id);
 	}
 
-	private static List<Item> extractItems(File zipFile, String pathPrefix) throws ZipException, IOException {
-		Collection<String> subEntries = ZipUtil.getSubEntries(zipFile, pathPrefix);
-		ArrayList<Item> r = new ArrayList<Item>();
-		for (String pathInZip : subEntries) {
-			String name = extractName(pathInZip);
-			String id = getId(name);
-			r.add(new Item(id, name));
-		}
-		return r;
+	public static Promise<Result> showAlgo(String id) {
+		return showDetail("algo", id);
 	}
 
-	private static String getId(String name) {
-		return name.replace(' ', '_');
-	}
-
-	public static Result showDs(String id) throws IOException, ParseException {
-		return showDetail(DS_PATH_PREFIX, id);
-	}
-
-	public static Result showAlgo(String id) throws IOException, ParseException {
-		return showDetail(ALGO_PATH_PREFIX, id);
-	}
-
-	private static Result showDetail(String pathPrefix, String id) throws ZipException, IOException, UnsupportedEncodingException, ParseException {
-		final String content = ZipUtil.loadUTF8StringInZipFileOrNull(getZipFile(), pathPrefix + id.replace("_", "") + EXAMPLE_FILE_SUFFIX);
-		AssertStatus.assertTrue(content != null);
-
-		CompilationUnit cu = JavaParser.parse(new ByteArrayInputStream(content.getBytes("UTF-8")), "UTF-8");
-		final DynamicArray<String> implementationSimpleClassName = DynamicArray.create();
-		final DynamicArray<String> seeAlsoClassName = DynamicArray.create();
-		new VoidVisitorAdapter<Object>() {
+	@SuppressWarnings("unchecked")
+	private static Promise<Result> showDetail(String categoryx, final String id) {
+		String exampleUrl = GITHUB_RAW_ROOT + "/" + EXAMPLE_PATH + "/" + categoryx + "/" + id.replace("_", "") + EXAMPLE_FILE_SUFFIX;
+		Promise<Response> example = createUrlFetchPromise(exampleUrl, EMPTY_PARAM);
+		Promise<Response> dsList = createUrlFetchPromise(DS_LIST_URL, createRefParam());
+		Promise<Response> algoList = createUrlFetchPromise(ALGO_LIST_URL, createRefParam());
+		return Promise.sequence(example, dsList, algoList).map(new Function<List<Response>, Result>() {
 			@Override
-			public void visit(ClassOrInterfaceDeclaration n, Object arg) {
-				JavadocComment docOrNull = n.getJavaDoc();
-				if (docOrNull == null)
-					return;
-				for (String line : StringUtil.toLines(docOrNull.getContent())) {
-					if (line.contains("@")) {
-						if (line.contains("@implementation")) {
-							implementationSimpleClassName.addToLast(extractSimpleClassNameFromTagLine(line));
-						} else if (line.contains("@see")) {
-							seeAlsoClassName.addToLast(extractSimpleClassNameFromTagLine(line));
-						} else {
-							throw new RuntimeException();
+			public Result apply(List<Response> list) throws Throwable {
+				final String content = list.get(0).getBody();
+				List<Item> ds = extractItems(list.get(1));
+				List<Item> algo = extractItems(list.get(2));
+
+				CompilationUnit cu = JavaParser.parse(new ByteArrayInputStream(content.getBytes("UTF-8")), "UTF-8");
+				final DynamicArray<String> implementationSimpleClassName = DynamicArray.create();
+				final DynamicArray<String> seeAlsoClassName = DynamicArray.create();
+				new VoidVisitorAdapter<Object>() {
+					@Override
+					public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+						JavadocComment docOrNull = n.getJavaDoc();
+						if (docOrNull == null)
+							return;
+						for (String line : StringUtil.toLines(docOrNull.getContent())) {
+							if (line.contains("@")) {
+								if (line.contains("@implementation"))
+									implementationSimpleClassName.addToLast(extractSimpleClassNameFromTagLine(line));
+								else if (line.contains("@see"))
+									seeAlsoClassName.addToLast(extractSimpleClassNameFromTagLine(line));
+								else
+									throw new RuntimeException();
+							}
 						}
 					}
-				}
-			}
-		}.visit(cu, null);
-		final DataKeeper<String> exampleBody = new DataKeeper<String>("");
-		new VoidVisitorAdapter<Object>() {
-			@Override
-			public void visit(BlockStmt stmt, Object arg1) {
-				String body = "";
-				Array<String> lines = StringUtil.toLines(content);
-				for (int i : ZeroTo.get(lines.size())) {
-					if (stmt.getBeginLine() <= i && i < stmt.getEndLine() - 1) {
-						String line = lines.get(i);
-						if (!line.trim().startsWith("Assert.assert")) {
-							if (line.startsWith("\t\t"))
-								body += line.substring(2) + "\n";
-							else
-								body += line + "\n";
+				}.visit(cu, null);
+				final DataKeeper<String> exampleBody = new DataKeeper<String>("");
+				new VoidVisitorAdapter<Object>() {
+					@Override
+					public void visit(BlockStmt stmt, Object arg1) {
+						String body = "";
+						Array<String> lines = StringUtil.toLines(content);
+						for (int i : ZeroTo.get(lines.size())) {
+							if (stmt.getBeginLine() <= i && i < stmt.getEndLine() - 1) {
+								String line = lines.get(i);
+								if (!line.trim().startsWith("Assert.assert")) {
+									if (line.startsWith("\t\t"))
+										body += line.substring(2) + "\n";
+									else
+										body += line + "\n";
+								}
+							}
 						}
+						AssertStatus.assertTrue(exampleBody.get().length() == 0);
+						exampleBody.set(body);
 					}
+				}.visit(cu, null);
+				AssertStatus.assertTrue(exampleBody.get().length() > 0);
+
+				DynamicArray<Pair<String, String>> impls = DynamicArray.create();
+				for (String s : implementationSimpleClassName) {
+					String className = getClassNameFromImport(content, s);
+					impls.addToLast(Pair.create(s, GITHUB_PAGE_ROOT + "/" + "src/main/java" + "/" + className.replace('.', '/') + ".java"));
 				}
-				AssertStatus.assertTrue(exampleBody.get().length() == 0);
-				exampleBody.set(body);
+
+				DynamicArray<Triple<String, String, String>> seeAlsos = DynamicArray.create();
+				for (String s : seeAlsoClassName) {
+					String name = extractName(s + ".java");
+					Item found = null;
+					for (Item item : ds)
+						if (item.name.equals(name))
+							found = item;
+					if (found != null)
+						seeAlsos.addToLast(Triple.create(name, "ds", found.id));
+					Item found2 = null;
+					for (Item item : algo)
+						if (item.name.equals(name))
+							found2 = item;
+					if (found2 != null)
+						seeAlsos.addToLast(Triple.create(name, "algo", found2.id));
+				}
+
+				return ok(detail.render(getName(id), exampleBody.get().trim(), Util.toList(impls), Util.toList(seeAlsos)));
 			}
-		}.visit(cu, null);
-		AssertStatus.assertTrue(exampleBody.get().length() > 0);
+		});
+	}
 
-		DynamicArray<Pair<String, String>> impls = DynamicArray.create();
-		for (String s : implementationSimpleClassName) {
-			String pathInZip = getEntryPath("/" + s + ".java");
-			impls.addToLast(Pair.create(s, "https://github.com/psjava/psjava/blob/" + REF_NAME + "/" + pathInZip.substring(PATH_PREFIX.length())));
+	private static Map<String, String> createRefParam() {
+		Map<String, String> param = new HashMap<String, String>();
+		param.put("ref", REF_NAME);
+		return param;
+	}
+
+	private static Promise<Response> createUrlFetchPromise(String url, Map<String, String> params) {
+		final String key = "http:" + url + ":" + new TreeMap<String, String>(params).toString();
+		final Response cached = (Response) Cache.get(key);
+		if (cached == null) {
+			WSRequestHolder holder = WS.url(url);
+			for (String k : params.keySet())
+				holder.setQueryParameter(k, params.get(k));
+			Promise<Response> promise = holder.get();
+			return promise.map(new Function<Response, Response>() {
+				@Override
+				public Response apply(Response res) throws Throwable {
+					Cache.set(key, res);
+					return res;
+				}
+			});
+		} else {
+			return Promise.promise(new Function0<Response>() {
+				@Override
+				public Response apply() throws Throwable {
+					return cached;
+				}
+			});
 		}
-
-		DynamicArray<Pair<String, String>> seeAlsos = DynamicArray.create();
-		for (String s : seeAlsoClassName) {
-			String pathInZip = getEntryPath("/" + s + ".java");
-			String name = extractName(pathInZip);
-			String category = pathInZip.substring(EXAMPLE_PATH_IN_ZIP.length(), pathInZip.lastIndexOf('/'));
-			String urlPath = category + "/" + getId(name);
-			seeAlsos.addToLast(Pair.create(name, urlPath));
-		}
-
-		return ok(detail.render(getName(id), exampleBody.get().trim(), Util.toList(impls), Util.toList(seeAlsos)));
 	}
 
 	private static String getName(String id) {
 		return id.replace('_', ' ');
 	}
 
-	private static String getEntryPath(String suffix) throws ZipException, IOException {
-		ZipFile z = new ZipFile(getZipFile());
-		String pathOrNull = null;
-		try {
-			Enumeration<? extends ZipEntry> entries = z.entries();
-			while (entries.hasMoreElements()) {
-				ZipEntry entry = entries.nextElement();
-				if (entry.getName().endsWith(suffix)) {
-					AssertStatus.assertTrue(pathOrNull == null);
-					pathOrNull = entry.getName();
-				}
-			}
-		} finally {
-			z.close();
-		}
-		AssertStatus.assertTrue(pathOrNull != null);
-		return pathOrNull;
-	}
-
-	protected static String extractName(String pathInZip) {
-		AssertStatus.assertTrue(pathInZip.endsWith(EXAMPLE_FILE_SUFFIX));
-		String sub = pathInZip.substring(pathInZip.lastIndexOf('/') + 1, pathInZip.length() - EXAMPLE_FILE_SUFFIX.length());
+	protected static String extractName(String fileName) {
+		AssertStatus.assertTrue(fileName.endsWith(EXAMPLE_FILE_SUFFIX));
+		String sub = fileName.substring(0, fileName.length() - EXAMPLE_FILE_SUFFIX.length());
 		return getCamelResolved(sub);
 	}
 
@@ -201,6 +227,34 @@ public class PsjavaSiteController extends Controller {
 		String temp = line.substring(line.indexOf("{@link")).trim();
 		temp = temp.replace("{@link", "").replace("}", "").trim();
 		return temp;
+	}
+
+	private static ArrayList<Item> extractItems(Response gitHubResponse) {
+		JSONArray dsJson = new JSONArray(gitHubResponse.getBody());
+		ArrayList<Item> r = new ArrayList<Item>();
+		for (int i : ZeroTo.get(dsJson.length())) {
+			String name = extractName(dsJson.getJSONObject(i).getString("name"));
+			String id = name.replace(' ', '_');
+			r.add(new Item(id, name));
+		}
+		return r;
+	}
+
+	private static String getClassNameFromImport(String content, String s) {
+		Array<String> lines = StringUtil.toLines(content);
+		for (String line : lines)
+			if (line.startsWith("import") && line.contains("." + s))
+				return line.substring(line.indexOf(' '), line.indexOf(';')).trim();
+		throw new RuntimeException("Cannot find class from import statement: " + s);
+	}
+
+	public static Promise<Result> license() {
+		return createUrlFetchPromise(GITHUB_RAW_ROOT + "/LICENSE", EMPTY_PARAM).map(new Function<Response, Result>() {
+			@Override
+			public Result apply(Response arg0) throws Throwable {
+				return ok(views.html.license.render(arg0.getBody()));
+			}
+		});
 	}
 
 }
